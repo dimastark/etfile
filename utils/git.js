8f89c20d87fb69480cb0c79adf58375a40163ce3
Дмитрий Старков <dstarkdev@gmail.com>
2017-06-20 21:43:58 +0500

    Run with repositories

--- utils/git.js
+++ utils/git.js
@@ -6,6 +6,13 @@ function repositoryExists(repoUrl, cb) {
     cp.exec(`git ls-remote ${repoUrl} HEAD`, error => cb(!error));
 }
 
+function getNameFromGitUrl(url) {
+    const gitNameReg = /([^/]+)\.git$/;
+
+    return gitNameReg.exec(url)[1];
+}
+
 module.exports = {
+    getNameFromGitUrl,
     repositoryExists
 };
