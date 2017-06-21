3aca4a2ea242b3471de36bde0cb2ca816e80d761
Дмитрий Старков <dstarkdev@gmail.com>
2017-06-21 19:05:35 +0500

    Settings

--- utils/settings.js
+++ utils/settings.js
@@ -2,11 +2,19 @@
 
 const fs = require('fs');
 const path = require('path');
+const trueRe = /^(yes|y|true)$/;
 
 module.exports = function readSettings() {
     const settingsPath = path.join(__dirname, '../services/settings.json');
     try {
-        return JSON.parse(fs.readFileSync(settingsPath));
+        const obj = JSON.parse(fs.readFileSync(settingsPath));
+
+        return {
+            smartMode: !!trueRe.exec(obj.smartMode),
+            needSquash: !!trueRe.exec(obj.needSquash),
+            pushFrequency: Number(obj.pushFrequency),
+            commitFrequency: Number(obj.commitFrequency),
+        };
     } catch (err) {
         console.error('Settings file not exists');
         process.exit(1);
