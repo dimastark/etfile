9a1fd09b00fad0510a851bf7497b690e02243d90
Дмитрий Старков <dstarkdev@gmail.com>
2017-06-21 19:45:30 +0500

    FINISH. NEED TEST

--- services/arbitration.js
+++ services/arbitration.js
@@ -14,10 +14,12 @@ const arbitrationService = new Service('arbitrator', {
 });
 const settings = readSettings();
 const fsServices = {};
+const commits = {};
 
 arbitrationService.responder.on('give me name', (req, cb) => {
     const name = nextName();
     fsServices[name] = { path: req.path, repo: req.repo, params: {} };
+    commits[name] = { count: 0, lastPath: '' };
     cb({ name });
 });
 
@@ -46,8 +48,9 @@ arbitrationService.responder.on('fs event', (req, cb) => {
             type: 'git add',
             path: fsServices[req.name].path
         });
+    } else {
+        handleEvent(req);
     }
-    console.log(req);
 });
 
 function nextName() {
@@ -57,4 +60,32 @@ function nextName() {
     }
 
     return name;
+}
+
+function handleEvent(event) {
+    const commitsObj = commits[event.name];
+    const fsServiceObj = fsServices[event.name];
+
+    const isSmartMode = settings.smartMode;
+    const isPathEqual = event.path === commitsObj.lastPath;
+    const needCommit = commitsObj.count % settings.commitFrequency === 0;
+    const needPush = commitsObj.count % settings.pushFrequency === 0;
+
+    if (isSmartMode && isPathEqual || !isSmartMode && needCommit) {
+        arbitrationService.requester.send({
+            type: 'git commit',
+            path: fsServiceObj.path
+        });
+    }
+
+    if (isSmartMode && !isPathEqual || !isSmartMode && needPush) {
+        arbitrationService.requester.send({
+            type: 'git push',
+            path: fsServiceObj.path
+        });
+    }
+
+    commitsObj.count += 1;
+    commitsObj.count %= settings.pushFrequency;
+    commits[event.name].lastPath = event.path;
 }
